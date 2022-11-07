7ebc709cf3dbccc81e5dfb31310fdddaaa72294a
abcd <abcd>
2022-11-07 19:46:35 +0600

    second commit

--- main.py
+++ main.py
@@ -66,6 +66,6 @@ def index():
 
 
 if __name__ == "__main__":
-    #app.run(host='127.0.0.1', port=5002)
+    app.run(host='127.0.0.1', port=5002)
     #serve(app, host='127.0.0.1', port=5002)
-    serve(app, host='0.0.0.0', port=80)
+    #serve(app, host='0.0.0.0', port=80)
